c4bd3006284c4d203fd6a5cdc73ff97fc67e23a4
Artem Nilov <nilov1235@mail.ru>
2022-04-24 17:12:16 +0300

    middle

--- lesson2/lesson2.cpp
+++ lesson2/lesson2.cpp
@@ -3,10 +3,35 @@
 
 #include <iostream>
 #include <string>
+#include <Windows.h>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello World2!" << std::endl;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	/*int a, b;
+	std::cout << "Enter a: " << std::endl;
+	std::cin >> a;
+	std::cout << "Enter b: " << std::endl;
+	std::cin >> b;
+	
+	std::cout << "Сумма a и b: " << a + b << std::endl;
+	std::cout << "Разность a и b: " << a - b << std::endl;
+	std::cout << "Произведение a и b: " << a * b << std::endl;
+	std::cout << "Деление a и b: " << (float)a / b << std::endl;
+	*/
+	/*
+	int a = 85, b = 14;
+	std::cout << a % b << std::endl;
+	a += b;
+	std::cout << a << std::endl;
+	a++;
+	std::cout << a << std::endl;
+	*/
+
+	int a = 7;
+	std::cout << a++ << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a-- << std::endl;
 
 }
